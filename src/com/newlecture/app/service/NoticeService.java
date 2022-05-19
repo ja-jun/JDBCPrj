4a33decfb73db2d24c7032e16de1fa67bd9ed624
ja-jun <86602435+ja-jun@users.noreply.github.com>
2022-05-19 15:56:58 +0900

    22.5.19
    update

---
+++ src/com/newlecture/app/service/NoticeService.java
@@ -0,0 +1,41 @@
+package com.newlecture.app.service;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Date;
+
+public class NoticeService {
+	public List<?> getList(){
+		
+		String url = "jdbc:oracle:thin:@localhost:1521/xepdb1";
+		String sql = "SELECT * FROM NOTICE";
+		
+		
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		Connection con = DriverManager.getConnection(url, "newlec", "ROOT");
+		Statement st = con.createStatement();
+		ResultSet rs = st.executeQuery(sql);
+		
+		while(rs.next()) {
+			int id = rs.getInt("ID");
+			String title = rs.getNString("TITLE");
+			String writerId = rs.getNString("WRITER_ID");
+			Date regDate = rs.getDate("REGDATE");
+			String content = rs.getString("CONTENT");
+			int hit = rs.getInt("hit");
+			
+				System.out.printf(" id:%d, title:%s, writerId:%s, regDate:%s, content:%s, hit:%d\n", 
+									id, title, writerId, regDate, content, hit);
+		}		
+	
+		rs.close();
+		st.close();
+		con.close();		
+		}
+	
+	 return?;
+	}
+
+}
